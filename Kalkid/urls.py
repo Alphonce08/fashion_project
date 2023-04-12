98212f460ce9c979cc4780f021dc5002fa5fa2d6
Aphonce08 <alphoncerotih30@gmail.com>
2023-04-12 23:53:45 +0300

    create home app/microapp

--- Kalkid/urls.py
+++ Kalkid/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('fashion', views.fashion, name='fashion'),
-    path('electronic', views.electronic, name='electronic'),
-    path('jewellery', views.jewellery, name='jewellery')
-]
+    path("", include("home.urls", namespace="home")),
+    
+    ]
