24ebd8c35d34f93043636bd4f38f02906229b1d0
Alphonce08 <alphoncerotich30@gmail.com>
2023-05-02 13:05:02 +0300

    login page

--- home/views.py
+++ home/views.py
@@ -34,6 +34,8 @@ def electronic(request):
 
 def fashion(request):
     return render(request, 'fashion.html')
+def login(request):
+    return render(request, 'login.html')
 
 def jewellery(request):
     return render(request, 'jewellery.html')
